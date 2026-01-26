0ed81b3fda1609a99165dabb67eb665dda36c2ca
usifk <me@usif.tech>
2026-01-26 16:07:25 +0200

    fix: update down_revision in migration for video addition to dive center

--- migrations/versions/d3c2bd806ebc_add_video_to_dive_center.py
+++ migrations/versions/d3c2bd806ebc_add_video_to_dive_center.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "d3c2bd806ebc"
-down_revision: Union[str, Sequence[str], None] = "7ed49fe7590f"
+down_revision: Union[str, Sequence[str], None] = "c153c1facfeb"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
