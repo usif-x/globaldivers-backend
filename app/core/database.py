a03f408a49da8f96a8c51a6eba350815b9025d97
usifk <me@usif.tech>
2026-06-26 00:45:04 +0300

    edit db conn utl

--- app/core/database.py
+++ app/core/database.py
@@ -1,24 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
 from app.core.config import settings
 
 # Engine
-engine = create_engine(
-    settings.DATABASE_URL,
-    echo=settings.DEBUG,
-    pool_pre_ping=True
-)
+db_url = settings.DATABASE_URL
 
+# إصلاح الصيغة إذا كانت تبدأ بـ postgres:// وتحويلها إلى postgresql://
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+# إنشاء المحرك باستخدام الرابط المعدل
+engine = create_engine(db_url, echo=settings.DEBUG, pool_pre_ping=True)
 # Session
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # Base Model
 class Base(DeclarativeBase):
     pass
+
 
 # Dependency (FastAPI style)
 def get_db():
